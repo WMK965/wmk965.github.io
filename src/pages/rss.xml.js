1dd8541da129c0b5e06156b3e5eef8d032f275fa
WMK965 <wmk153024@gmail.com>
2024-12-05 14:38:03 +0800

    Deploy Channel Sites

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,42 @@
+import rss from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
+import { getChannelInfo } from '../lib/telegram'
+
+export async function GET(Astro) {
+  const { SITE_URL } = Astro.locals
+  const tag = Astro.url.searchParams.get('tag')
+  const channel = await getChannelInfo(Astro, {
+    q: tag ? `#${tag}` : '',
+  })
+  const posts = channel.posts || []
+
+  const request = Astro.request
+  const url = new URL(request.url)
+  url.pathname = SITE_URL
+  url.search = ''
+
+  return rss({
+    title: `${tag ? `${tag} | ` : ''}${channel.title}`,
+    description: channel.description,
+    site: url.origin,
+    trailingSlash: false,
+    items: posts.map(item => ({
+      link: `posts/${item.id}`,
+      title: item.title,
+      description: item.description,
+      pubDate: new Date(item.datetime),
+      content: sanitizeHtml(item.content, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'video', 'audio']),
+        allowedAttributes: {
+          ...sanitizeHtml.defaults.allowedAttributes,
+          video: ['src', 'width', 'height', 'poster'],
+          audio: ['src', 'controls'],
+          img: ['src', 'srcset', 'alt', 'title', 'width', 'height', 'loading', 'class'],
+        },
+        exclusiveFilter(frame) {
+          return frame.tag === 'img' && frame.attribs?.class?.includes('modal-img')
+        },
+      }),
+    })),
+  })
+}
